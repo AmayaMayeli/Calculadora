ede5958aeb306807781a8e953d79aec3ecd2f6ed
Alexis Vasz <61213709+AlexisVasz@users.noreply.github.com>
2020-09-03 09:12:34 -0600

    Main principal del proyecto calculadora.
    Es la estructura básica del programa, hacen falta las operaciones básicas.

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int variable1, variable2, OpcionMenu;
+    cout << "Ingrese el numero que corresponda segun la operacion deseada:" << endl;
+    cout<<"1. Suma"<<endl;
+    cout<<"2. Resta"<<endl;
+    cout<<"3. Multiplicacion"<<endl;
+    cout<<"4. Division"<<endl;
+
+    if (OpcionMenu == 1)
+    {
+
+    }
+    else if (OpcionMenu == 2)
+    {
+
+    }
+    else if (OpcionMenu == 3)
+    {
+
+    }
+    else if (OpcionMenu == 4)
+    {
+
+    }
+    else
+    {
+        cout<<"Error, valor no admitido"<<end;
+    }
+    return 0;
+}
