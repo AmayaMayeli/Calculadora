b2a713fb8bb55c5bc06fa1297574b44a318ed5be
Carlos Cancinos <61300585+carloscancinos12@users.noreply.github.com>
2020-09-03 09:27:31 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
+int  main ()
 {
-    int variable1, variable2, OpcionMenu;
-    cout << "Ingrese el numero que corresponda segun la operacion deseada:" << endl;
-    cout<<"1. Suma"<<endl;
-    cout<<"2. Resta"<<endl;
-    cout<<"3. Multiplicacion"<<endl;
-    cout<<"4. Division"<<endl;
-
-    if (OpcionMenu == 1)
+    int variable1, variable2, OpcionMenu,numero1,numero2,resultado;
+    cout << " Ingrese el numero que corresponda segun la operacion deseada: " << endl;
+    cout << " 1. Suma " << endl;
+    cout << " 2. Resta " << endl;
+    cout << " 3. Multiplicacion " << endl;
+    cout << " 4. División " << endl;
+    cin >> OpcionMenu;
+    if (OpcionMenu == 1 )
     {
-
+    cout << "ingrese el primer numero" << endl;
+    cin >> numero1;
+    cout << "ingrese el segundo numero" << endl;
+    cin >> numero2;
+    resultado = numero1 + numero2;
+    cout << "La suma es igual a: "<< resultado << endl;
     }
-    else if (OpcionMenu == 2)
+    else if (OpcionMenu == 2 )
     {
-
+    cout << "ingrese el primer numero" << endl;
+    cin >> numero1;
+    cout << "ingrese el segundo numero" << endl;
+    cin >> numero2;
+    resultado = numero1 - numero2;
+    cout<< "La diferencia es igual a: "  << resultado << endl;
     }
-    else if (OpcionMenu == 3)
+    else if (OpcionMenu == 3 )
     {
-
+    cout << "ingrese el primer numero" << endl;
+    cin >> numero1;
+    cout << "ingrese el segundo numero" << endl;
+    cin >> numero2;
+    resultado = numero1 * numero2;
+    cout<< "Producto es igual a: "  << resultado << endl;
     }
-    else if (OpcionMenu == 4)
+    else if (OpcionMenu == 4 )
     {
-
+    cout << "ingrese el primer numero" << endl;
+    cin >> numero1;
+    cout << "ingrese el segundo numero" << endl;
+    cin >> numero2;
+    resultado = numero1 / numero2;
+    cout << "El cociente es igual a: " << resultado << endl;
     }
     else
     {
-        cout<<"Error, valor no admitido"<<end;
+        cout << " Error, valor no admitido ";
     }
-    return 0;
+    return  0 ;
 }
